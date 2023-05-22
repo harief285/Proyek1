6a665a4f8c3d2a291191a1813eab5bdffe3d0750
rayhangrdp <mr2021649.gmail.com>
2023-05-22 20:33:39 +0700

    mahasiswa3.php

---
+++ app/Http/Controllers/mahasiswa3.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class mahasiswa3Controller extends Controller
+{
+    
+    public function index(){
+        $namakelompok="kelompok 6";
+        $title="straightface.com";
+        $slug="straightface";
+    
+return view('konten.mahasiswa',compact('namakelompok','title', 'slug',));
+
+
+
+
+    }
+
+}
+
+
