00f277b3194cfbc36b3f32f2dd9d0f34d3993bd0
rayhangrdp <mr2021649.gmail.com>
2023-06-04 01:15:53 +0700

    main7.blade.php

--- resources/views/konten/main7.blade.php
+++ resources/views/konten/main7.blade.php
@@ -1,8 +1,11 @@
+
+
+<a href="{{asset('WhatsApp1.jpeg ')}}">
 <div class="right_col" role="main7">
 
     <p>bisa klik saja </p>
     <h1>Instragram</h1>
-
+    
 
     
     <a href="https://instagram.com/stgfc_ind?igshid=NTc4MTIwNjQ2YQ== "> link straight face 
