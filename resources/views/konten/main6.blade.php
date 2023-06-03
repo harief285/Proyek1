0fe5ef764bc5f006df3e7a785e1ae9465ae2e81e
rayhangrdp <mr2021649.gmail.com>
2023-06-03 18:10:33 +0700

    main6.blade.php

--- resources/views/konten/main6.blade.php
+++ resources/views/konten/main6.blade.php
@@ -2,13 +2,16 @@
 
 <div class="right_col" role="main6">
 
+    <a href="{{asset('WhatsApp.jpeg ')}}">
+
+
+
     <h1>WhatsApp
 
 
 
     </h1>
 
-    <a href="{{asset('whatsaap.png')}}">
 
 <a href="http://wa.me//+6281395019382" >hubungi saya +6281395019382 </a>
 
