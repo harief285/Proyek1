e4db64798aafd0790bd7dac8bdce34370562c22d
raudatul18 <raudatulmusyrofahmaulaisjati@gmail.com>
2023-06-06 14:54:06 +0700

    main4.blade

--- resources/views/konten/main4.blade.php
+++ resources/views/konten/main4.blade.php
@@ -1,10 +1,4 @@
-<div class="right_col" role="main5">
-
-
-    <h1>category<h1>
-
-        </h1>kaos</h1>
-        <a href="{{asset('background7.jpeg')}}">
+<a href="{{asset('background7.jpeg')}}">
 
 
 
